b5ffb85fea48cdb2d38f38022d2448204c38c60f
HEHABUSTb <msanvold@gmail.com>
2021-11-01 13:23:46 +0300

    add transition to login page

--- test_main_page.py
+++ test_main_page.py
@@ -6,7 +6,9 @@ def test_guest_can_go_to_login_page(browser):
     browser.get(link)
     page = MainPage(browser, link) # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open() #open page
-    page.go_to_login_page() #go to login page
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
 
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
