017d5aa416cb2280da513bcc4c6b2d3ed20a5464
HEHABUSTb <msanvold@gmail.com>
2021-11-04 14:23:51 +0300

    add test_product_page.py add new locators add new page object in product_page.py

--- pages/locators.py
+++ pages/locators.py
@@ -7,3 +7,10 @@ class MainPageLocators():
 class LoginPageLocators():
     login_form = (By.CSS_SELECTOR, '#login_form')
     register_form = (By.CSS_SELECTOR, '#register_form')
+
+class ProductPageLocators():
+    add_basket = (By.CSS_SELECTOR, '.btn-add-to-basket')
+    price_product = (By.CSS_SELECTOR, '.price_color:nth-child(2)')
+    price_in_basket = (By.CSS_SELECTOR, '.alert .alertinner p strong')
+    product_name = (By.CSS_SELECTOR, '.product_main h1')
+    product_name_in_basket = (By.CSS_SELECTOR, '.alert .alertinner strong')
---
+++ pages/product_page.py
@@ -0,0 +1,24 @@
+from .base_page import BasePage
+from selenium.webdriver.common.by import By
+from .locators import ProductPageLocators
+
+class ProductPage(BasePage):
+    def add_product_to_basket(self):
+        add_to_basket = self.browser.find_element(*ProductPageLocators.add_basket)
+        add_to_basket.click()
+
+    def should_be_correct_product_price_in_basket(self):
+        price = self.browser.find_element(*ProductPageLocators.price_product).text
+        basket_price = self.browser.find_element(*ProductPageLocators.price_in_basket).text
+        #print('price = ', price, 'type of price =', type(price))
+        #print('price = ', basket_total, 'type of price =', type(basket_total))
+        assert price == basket_price, 'Price in basket not equal price of product'
+
+    def should_be_correct_product_name_in_basket(self):
+        product_name = self.browser.find_element(*ProductPageLocators.product_name).text
+        product_name_in_basket = self.browser.find_element(*ProductPageLocators.product_name_in_basket).text
+        #print('Name = ', product_name, 'type of product name =', type(product_name))
+        #print('Name = ', product_name_in_basket, 'type of in basket =', type(product_name_in_basket))
+        #print(product_name == product_name_in_basket)
+        assert  product_name == product_name_in_basket, 'Product name and name in basket is different'
+
