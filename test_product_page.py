882d5780c57ef844ef09b6adad7f4a496a830ccc
HEHABUSTb <msanvold@gmail.com>
2021-11-04 15:43:08 +0300

    add more links how pytest.mark.parametrize

--- test_product_page.py
+++ test_product_page.py
@@ -1,16 +1,27 @@
+import pytest
 from .pages.product_page import ProductPage
 import time
 
-def test_guest_can_add_product_to_basket(browser):
-    link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear'
-    link = 'http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/?promo=newYear2019'
-    product_page = ProductPage(browser, link)
+links =  ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+          "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+          "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+          "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+          "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+          "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+          "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+          "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
+          "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+          "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"]
 
+@pytest.mark.parametrize('link', links)
+def test_guest_can_add_product_to_basket(browser, link):
+    product_page = ProductPage(browser, link)
     product_page.open()
     product_page.add_product_to_basket() #add product to basket
     time.sleep(1)
-
     product_page.solve_quiz_and_get_code() #solve puzzle
+
+
     product_page.should_be_correct_product_price_in_basket()  #Check price in basket
     product_page.should_be_correct_product_name_in_basket() #Check name of product in basket
     time.sleep(1)
