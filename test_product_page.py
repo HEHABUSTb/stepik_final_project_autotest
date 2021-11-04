aaee3b77f417f3909041cec8b5d77757f9a56cad
HEHABUSTb <msanvold@gmail.com>
2021-11-04 15:13:21 +0300

    add new link for check independence css selectors

--- test_product_page.py
+++ test_product_page.py
@@ -3,6 +3,7 @@ import time
 
 def test_guest_can_add_product_to_basket(browser):
     link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear'
+    link = 'http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/?promo=newYear2019'
     product_page = ProductPage(browser, link)
 
     product_page.open()
