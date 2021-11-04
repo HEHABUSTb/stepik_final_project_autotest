d396908b50a9f849de2625aa3f90ef33ea8874bc
HEHABUSTb <msanvold@gmail.com>
2021-11-04 15:58:46 +0300

    delete time.sleep

--- test_product_page.py
+++ test_product_page.py
@@ -20,10 +20,9 @@ def test_guest_can_add_product_to_basket(browser, link):
     product_page = ProductPage(browser, link)
     product_page.open()
     product_page.add_product_to_basket() #add product to basket
-    time.sleep(1)
+
     product_page.solve_quiz_and_get_code() #solve puzzle
 
 
     product_page.should_be_correct_product_price_in_basket()  #Check price in basket
     product_page.should_be_correct_product_name_in_basket() #Check name of product in basket
-    time.sleep(1)
